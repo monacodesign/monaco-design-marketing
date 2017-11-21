f0014108eeada3efe27a32fed98cbb3c3b655af2
monacodesign <33849302+monacodesign@users.noreply.github.com>
2017-11-20 20:19:58 -0400

    add files via upload
    all the files were added to the file

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*////Monaco////
+*/
+
+$(document).ready(function(){
+
+
+console.log(("Its working"));
+
+
+
+
+//id selector
+$("#close,#hamburger").click(function(){
+  $("nav ul").toggleClass("open-nav");
+
+$("#close,#hamburger").toggle()
+//
+// alert("this was clicked!");
+
+// /code to be executed on click
+
+
+
+
+
+
+})
+})
